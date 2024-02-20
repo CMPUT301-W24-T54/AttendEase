a897ab57084e4ecec6e6c48947020479661b3a0a
Sean Hill <srhill1@ualberta.ca>
2024-02-19 17:07:37 -0700

    test

--- AttendEase/app/src/main/java/com/example/attendease/MainActivity.java
+++ AttendEase/app/src/main/java/com/example/attendease/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.attendease;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-// test push
+// test
 public class MainActivity extends AppCompatActivity {
 
     @Override
