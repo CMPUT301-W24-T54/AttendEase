c601c44d075ce0ecb61085207a10c4b66bd27971
Rushabh Shah <rnshah@ualberta.ca>
2024-02-20 18:11:26 -0700

    Added launch page and admin log in

---
+++ AttendEase/app/src/main/java/com/example/attendease/AdminAccountActivity.java
@@ -0,0 +1,36 @@
+package com.example.attendease;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+public class AdminAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_account);
+
+        Button backButton = findViewById(R.id.back_button);
+        Button loginIn = findViewById(R.id.login_button);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminAccountActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        loginIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Logic upon clicking LOG IN!
+            }
+        });
+
+    }
+}
